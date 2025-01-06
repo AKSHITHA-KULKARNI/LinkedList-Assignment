ce930771df88f2f55626ca0fbcce55217ef1480c
Akshitha Kulkarni <akshitha.k03@gmail.com>
2025-01-06 16:17:57 +0530

    added: appends element functionality

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,4 +1,38 @@
-package PACKAGE_NAME;
+class LinkedList<T extends Comparable<T>> {
+    private Node<T> head;
 
-public class LinkedList {
+    // UC1: Create a simple linked list
+    public void add(T data) {
+        Node<T> newNode = new Node<>(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node<T> temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+    }
+    // UC2: Add elements to the beginning of the list
+    public void addFirst(T data) {
+        Node<T> newNode = new Node<>(data);
+        newNode.next = head;
+        head = newNode;
+    }
+
+    // UC3: Append elements to the list
+    public void append(T data) {
+        add(data);
+    }
+
+
+    public void printList() {
+        Node<T> temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
 }
